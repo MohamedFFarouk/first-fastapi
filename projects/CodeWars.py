6e8e95b34ac46134f53c0dbd5778b545345afe1c
Mohmed fawaz <mohamedffaroukg@gmail.com>
2025-06-17 21:58:21 +0300

    added env variabls

--- projects/CodeWars.py
+++ projects/CodeWars.py
@@ -1,5 +1,8 @@
-def sort_twisted37(arr):
-    def twist_number(n):
-        return int(str(n).replace("3", "x").replace("7", "3").replace("x", "7"))
+from collections import Counter
 
-    return sorted(arr, key=twist_number)
+
+def is_language_diverse(lst):
+    language_counts = Counter(dev["language"] for dev in lst)
+    max_count = max(language_counts.values())
+    min_count = min(language_counts.values())
+    return max_count <= 2 * min_count
